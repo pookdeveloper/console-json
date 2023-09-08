590a9d2114f82d2e42d8d67b30beba47501c2b2a
David <ddgs@gft.com>
2023-09-08 11:45:50 +0200

    format code

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
+
+export function cjson(json: any, text_group = "JSON Formatted"): void {
+  console.log(this.fjson(json);
+}
+
 export function cgjson(json: any, text_group = "JSON Formatted"): void {
   console.groupCollapsed(text_group);
-  console.log(JSON.stringify(json, null, "\t"));
+  console.log(this.fjson(json);
   console.groupEnd();
-  console.log(JSON.stringify(json, null, "\t"));
 }
 
 export function fjson(json: any): any {
   return JSON.stringify(json, null, "\t");
 }
 
+
 module.exports = {
+  cjson,
   cgjson,
-  fjson,
+  fjson
 };
